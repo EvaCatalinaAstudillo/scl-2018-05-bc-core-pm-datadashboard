7e1c8ba0d89937ee8ba3255fbdd780a27f089855
AriellaRogueN <maitilydevidasi@gmail.com>
2018-06-21 09:24:40 -0400

    agregue opcion de error para los fecth y funcion de ordenar

--- src/js/data.js
+++ src/js/data.js
@@ -1,35 +1,38 @@
-window.onload = function () {
+window.onload = function() {
   requiereJson();
 };
 async function requiereJson() {
-  const jsonUsers = await fetch('../../data/cohorts/lim-2018-03-pre-core-pw/users.json');
-  const users = await jsonUsers.json();
-  const jsonProgress = await fetch('../../data/cohorts/lim-2018-03-pre-core-pw/progress.json');
-  const progress = await jsonProgress.json();
-  const jsonCohorts = await fetch('../../data/cohorts.json');
-  const cohorts = await jsonCohorts.json();
+  try {
+    const jsonUsers = await fetch('../../data/cohorts/lim-2018-03-pre-core-pw/users.json');
+    const users = await jsonUsers.json();
+    const jsonProgress = await fetch('../../data/cohorts/lim-2018-03-pre-core-pw/progress.json');
+    const progress = await jsonProgress.json();
+    const jsonCohorts = await fetch('../../data/cohorts.json');
+    const cohorts = await jsonCohorts.json();
 
-  showCohortsList(cohorts);
-  inputChange(users, progress, cohorts);
+    showCohortsList(cohorts);
+    inputChange(users, progress, cohorts);
+  } catch (err) {
+    alert('no se pudierón cargar los datos' + err);
+  }
 }
-
 inputChange = (users, progress, cohorts) => {
   let searchString = '';
   let findCohort = '';
   let filter = '';
   let direcction = '';
 
-  //Escuchar eventos del dom y llamar la funcion cada vez que se cambie el filtro
-  document.getElementById('cohortsInput').addEventListener('change', function () {
+  // Escuchar eventos del dom y llamar la funcion cada vez que se cambie el filtro
+  document.getElementById('cohortsInput').addEventListener('change', function() {
     let cohortSelect = document.getElementById('cohortsInput').value;
     findCohort = cohorts.find(item => item.id === cohortSelect);
     printData(processCohortData(createObjectOptions()));
   });
-  document.getElementById('searchButtom').addEventListener('click', function () {
+  document.getElementById('searchButtom').addEventListener('click', function() {
     searchString = document.getElementById('searchInput').value;
     printData(processCohortData(createObjectOptions()));
   });
-  document.getElementById('filterButtom').addEventListener('click', function () {
+  document.getElementById('filterButtom').addEventListener('click', function() {
     filter = document.getElementById('filterInput').value;
     direcction = document.filterForm.direction.value;
     printData(processCohortData(createObjectOptions()));
@@ -48,21 +51,72 @@ inputChange = (users, progress, cohorts) => {
     return options;
   };
 };
-processCohortData = (options) => {
 
+processCohortData = (options) => {
   let coursesCohortSelect = Object.keys(options.cohort.coursesIndex);
   let userNewArray = computeUsersStats(options.cohortData.users, options.cohortData.progress, coursesCohortSelect);
   userNewArray = filterUsers(userNewArray, options.search);
   userNewArray = sortUsers(userNewArray, options.orderBy, options.orderDirection);
   return userNewArray;
 };
-sortUsers = (users, orderBy, orderDirection) => {
-//aca pongan el codigo
+window.sortUsers = (users, orderBy, orderDirection) => {
+  let sorted = users;
+
+  if (orderBy === 'Nombre') {
+    if (orderDirection === 'ASC') {
+      sorted = users.sort((one, two) => one.name.localeCompare(two.name));
+    }
+    if (orderDirection === 'DESC') {
+      sorted = users.sort((one, two) => one.name.localeCompare(two.name) * -1);
+    }
+  }
+
+  if (orderBy === 'Porcentaje de completitud total') {
+    if (orderDirection === 'ASC') {
+      sorted = users.sort((one, two) => one.stats.percent - two.stats.percent);
+    }
+    if (orderDirection === 'DESC') {
+      sorted = (users.sort((one, two) => one.stats.percent - two.stats.percent)).reverse();
+    }
+  }
+
+  if (orderBy === 'Porcentaje de ejercicios autocorregidos completados') {
+    if (orderDirection === 'ASC') {
+      sorted = users.sort((one, two) => one.stats.exercises.percent - two.stats.exercises.percent);
+    }
+    if (orderDirection === 'DESC') {
+      sorted = (users.sort((one, two) => one.stats.exercises.percent - two.stats.exercises.percent)).reverse();
+    }
+  }
+  if (orderBy === 'Porcentaje de quizzes completados') {
+    if (orderDirection === 'ASC') {
+      sorted = users.sort((one, two) => one.stats.quizzes.percent - two.stats.quizzes.percent);
+    }
+    if (orderDirection === 'DESC') {
+      sorted = (users.sort((one, two) => one.stats.quizzes.percent - two.stats.quizzes.percent)).reverse();
+    }
+  }
+  if (orderBy === 'Puntuación promedio en quizzes completados') {
+    if (orderDirection === 'ASC') {
+      sorted = users.sort((one, two) => one.stats.quizzes.scoreAvg - two.stats.quizzes.scoreAvg);
+    }
+    if (orderDirection === 'DESC') {
+      sorted = (users.sort((one, two) => one.stats.quizzes.scoreAvg - two.stats.quizzes.scoreAvg)).reverse();
+    }
+  }
+  if (orderBy === 'Porcentaje de lecturas completadas') {
+    if (orderDirection === 'ASC') {
+      sorted = users.sort((one, two) => one.stats.reads.percent - two.stats.reads.percent);
+    }
+    if (orderDirection === 'DESC') {
+      sorted = (users.sort((one, two) => one.stats.reads.percent - two.stats.reads.percent)).reverse();
+    }
+  }
+  return sorted;
 };
-filterUsers = (users, search) => {
-//aca pongan el codigo
-  
+window.filterUsers = (users, search) => {
+ 
 };
-computeUsersStats = (users, progress, courses) => {
-//aca pongan el codigo
+window.computeUsersStats = (users, progress, courses) => {
+
 };
